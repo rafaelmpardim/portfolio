1560610c9a5139802a5c63865be9950c38c9e7c0
rafaelmpardim <rafaelmedeirospardim@gmail.com>
2022-11-03 19:11:26 -0300

    feat: structuring project

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+	return (
+		<Html>
+			<Head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
+}
