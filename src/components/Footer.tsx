8923fd010881570315f8b6b590947b29c40fb45a
rafaelmpardim <rafaelmedeirospardim@gmail.com>
2022-11-10 10:29:01 -0300

    feat: creating Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,29 @@
+import { Link } from './Link'
+
+export function Footer() {
+	return (
+		<footer
+			className='flex items-center justify-center max-w-[1240px] mx-auto mt-16 py-6'
+		>
+			<ul
+				className='flex items-center gap-8'
+			>
+				<li>
+					<Link href='https://github.com/rafaelmpardim'>
+						github
+					</Link>
+				</li>
+				<li>
+					<Link href='https://intagram.com/rafaelmpardim'>
+						instagram
+					</Link>
+				</li>
+				<li>
+					<Link href='https://linkedin.com/in/rafaelmpardim'>
+						linkedin
+					</Link>
+				</li>
+			</ul>
+		</footer>
+	)
+}
