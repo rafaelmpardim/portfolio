c27bdb665e81d0e4966e26edddba116f2612f4e7
rafaelmpardim <rafaelmedeirospardim@gmail.com>
2022-11-10 10:28:20 -0300

    feat: creating Next Document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,10 @@ export default function Document() {
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
 
 				<link href="https://fonts.googleapis.com/css2?family=Saira:wght@100;400;500;700&display=swap" rel="stylesheet" />
+
+				<link rel="shortcut icon" href="favicon.png" type="image/png" sizes='32x32'/>
 			</Head>
-			<body className='bg-red-900'>
+			<body	className='bg-black text-gray-100'>
 				<Main />
 				<NextScript />
 			</body>
