391819c2d5bb2b04e41880eb5087d06ca875d59b
rafaelmpardim <rafaelmedeirospardim@gmail.com>
2022-11-10 10:29:45 -0300

    feat: creating Link component

---
+++ src/components/Link.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react'
+
+import NextLink, { LinkProps as NextLinkProps } from 'next/link'
+
+import clsx from 'clsx'
+
+interface LinkProps extends NextLinkProps {
+  children: ReactNode
+  className?: string
+}
+
+export function Link({ children, className, ...rest }: LinkProps) {
+	return (
+		<NextLink
+			className={
+				clsx(
+					'text-cyan-900 hover:text-cyan-950 transition-colors',
+					className
+				)
+			}
+			{...rest}
+		>
+			{children}
+		</NextLink>
+	)
+}
